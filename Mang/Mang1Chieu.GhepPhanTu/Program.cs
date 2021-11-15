71e95c50e13ffecde990e6a87e5567bf0a1ba56b
Nguyen Phan Du <dunp@omt.vn>
2021-11-15 11:16:12 +0700

    GhepJagedAnd1Chieu

--- Mang/Mang1Chieu.GhepPhanTu/Program.cs
+++ Mang/Mang1Chieu.GhepPhanTu/Program.cs
@@ -11,7 +11,7 @@ namespace Mang1Chieu.GhepPhanTu
         {
             Console.WriteLine("Hello World!");
             // có thể thay đổi số lượng cũng như phần tử ở mảng a,b để kiểm tra kết quảA
-            int[] a = { 1, 3, 5, 4 };
+            int[] a = { 1, 3 };
             int[] b = { 7, 8 };
             int[] d = { 9, 0 };
             Mang1Chieu mang1Chieu = new Mang1Chieu();
@@ -20,7 +20,7 @@ namespace Mang1Chieu.GhepPhanTu
 
             int[] c = { 100, 200 };
 
-            var ketqua1 = new Mang1Chieu().GhepCacMang1Chieu(new int[][] { a, b, c, d });
+            var ketqua1 = new Mang1Chieu().GhepCacMang1Chieu(new int[][] { a, b, c});
             Display(ketqua1);
 
         }
@@ -62,47 +62,68 @@ namespace Mang1Chieu.GhepPhanTu
             }
             return jagged_arr;
         }
-        int[][] jagged_arr_ = new int[][] { };
-        int[][] jagged_arr = new int[][] { };
-        public int[][] GhepCacMang1Chieu(int[][] inputs)
+
+        public int[][] GhepJagedAnd1Chieu(int[][] inputs, int[] mang2)
         {
-            int i = 1;
+            int jaggedLenth = 1;
             if (inputs != null && inputs.Count() > 0)
             {
                 foreach (var item in inputs)
                 {
-                    i = i * item.Length;
+                    jaggedLenth = jaggedLenth * item.Length;
                 }
             }
+            int[][] jagged_arr = new int[jaggedLenth][];
 
-
-            int x = 0;
-            if (inputs != null && inputs.Count() > 0)
+            foreach (var i2 in mang2)
             {
-                this.jagged_arr_ = new int[inputs[0].Length * inputs[1].Length][];
-                jagged_arr_ = Ghep2Mang1Chieu(inputs[0], inputs[1]);
-                jagged_arr = new int[i][];
-                for (int ikk = 2; ikk < inputs.Count(); ikk++)
+                foreach(var j in inputs)
                 {
-                    x = 0;
-                    foreach (var item in this.jagged_arr_)
+                    foreach( var itm in j)
                     {
 
-                        foreach (var itemi in inputs[ikk])
-                        {
-                            if (jagged_arr[x] == null)
-                            {
-                                this.jagged_arr[x] = item.Concat(new int[] { itemi }).ToArray();
-                            }
-                            else
-                            {
-                                this.jagged_arr[x] = this.jagged_arr[x].Concat(new int[] { itemi }).ToArray();
-                            }
-                            x += 1;
-                        }
                     }
                 }
             }
+            
+        }
+
+
+        int[][] jagged_arr_ = new int[][] { };
+        int[][] jagged_arr = new int[][] { };
+        public int[][] GhepCacMang1Chieu(int[][] inputs)
+        {
+            //Dictionary<int, int[]> indexMappingInputs = new Dictionary<int, int[]>();
+
+            if (inputs.Length <= 1) return inputs;
+
+            int jaggedLenth = 1;
+            if (inputs != null && inputs.Count() > 0)
+            {
+                foreach (var item in inputs)
+                {
+                    jaggedLenth = jaggedLenth * item.Length;
+                }
+            }
+
+
+            int[][] jagged_arr = new int[jaggedLenth][];
+            int[][] temp = new int[0][];
+
+            for (var i =0;i< jaggedLenth-2; i++)
+            {
+                if(i==0)
+                {
+                    temp = Ghep2Mang1Chieu(inputs[i], inputs[i + 1]);
+                }
+                else
+                {
+                    temp = GhepJagedAnd1Chieu(temp, inputs[i + 2]);
+                }
+            }
+
+            
+          
             return jagged_arr;
         }
     }
